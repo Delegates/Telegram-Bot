cdd6f50605ac8f57e377180db9cf1b2ae5c9f093
playstation4 <teppopp@mail.ru>
2016-05-27 01:51:50 +0500

    Обёртка над Api

---
+++ TelegramBot/Bot/MessageHandler/LocationMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using File = System.IO.File;
+
+namespace TelegramBot.Handler
+{
+    public class LocationMessageHandler : IMessageHandler
+    {
+        public void ProcessTheMessage(Message message, IApi api, User user)
+        {
+            api.SendTextMessage(message.Chat.Id, "wow such map much location");
+            api.SendPhoto(message.Chat.Id, new FileToSend("123", new FileStream("1.jpg", FileMode.Open)));
+        }
+    }
+}
---
+++ TelegramBot/Bot/MessageHandler/TextMessageHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bot;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.Handler
+{
+    public class TextMessageHandler :  IMessageHandler
+    {
+        private string commandPrefix;
+
+
+        private readonly List<ICommand> commandList;
+        private readonly Dictionary<string, ICommand> nameToCommand = new Dictionary<string, ICommand>();
+
+
+        public TextMessageHandler(string prefix = "/")
+        {
+            commandPrefix = prefix;
+            commandList = new List<ICommand>();
+        }
+
+        public TextMessageHandler(params ICommand[] commands)
+        {
+            commandList = new List<ICommand>();
+            AddCommands(commands);
+        }
+
+        public TextMessageHandler AddCommands(params ICommand[] commands)
+        {
+            foreach (var command in commands)
+            {
+                commandList.Add(command);
+                nameToCommand.Add(command.Name, command);
+            }
+            return this;
+        }
+
+        public IReadOnlyList<ICommand> CommandList => commandList.AsReadOnly();
+
+
+        public void ProcessTheMessage(Message message, IApi api, User user)
+        {
+            if (message.Text == null || !message.Text.StartsWith(commandPrefix) || message.Text.Length==1) return;
+            var text = message.Text;
+            text = text.EndsWith($"@{user.Username}") ? text.Substring(commandPrefix.Length, text.Length - 1 - user.Username.Length - 1) : text.Substring(commandPrefix.Length);
+            var command = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            api.SendTextMessage(message.Chat.Id, ExecuteCommand(command[0], command.Skip(1).ToArray()));
+        }
+
+        public string ExecuteCommand(string command, params string[] args)
+        {
+            var cmd = command.ToLower();
+            if (!nameToCommand.ContainsKey(cmd)) return "WoW so unusual much unknown letters";
+            return nameToCommand[cmd].Execute(args);
+        }
+    }
+}
